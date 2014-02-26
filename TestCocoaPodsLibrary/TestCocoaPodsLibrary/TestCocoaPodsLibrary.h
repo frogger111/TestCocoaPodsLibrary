54cbf4837fe76867773a2d540058d0c7ec9094ca
frogger111 <tobiasz.parys@gmail.com>
2014-02-26 15:05:12 +0100

    Initial commit

---
+++ TestCocoaPodsLibrary/TestCocoaPodsLibrary/TestCocoaPodsLibrary.h
@@ -0,0 +1,13 @@
+//
+//  TestCocoaPodsLibrary.h
+//  TestCocoaPodsLibrary
+//
+//  Created by Tobiasz Parys on 26/02/14.
+//  Copyright (c) 2014 Tobiasz Parys. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestCocoaPodsLibrary : NSObject
+
+@end
---
+++ TestCocoaPodsLibrary/TestCocoaPodsLibrary/TestCocoaPodsLibrary.m
@@ -0,0 +1,13 @@
+//
+//  TestCocoaPodsLibrary.m
+//  TestCocoaPodsLibrary
+//
+//  Created by Tobiasz Parys on 26/02/14.
+//  Copyright (c) 2014 Tobiasz Parys. All rights reserved.
+//
+
+#import "TestCocoaPodsLibrary.h"
+
+@implementation TestCocoaPodsLibrary
+
+@end
